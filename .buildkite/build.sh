0aa5db893a2e525f897fb36d48c11f35daea84f1
Emil Ingerslev <emil@ingerslev.io>
2018-02-07 10:29:26 +0100

    Fix build

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "--- Build image"
 eval "$($(dirname $0)/env.sh)"
+
+echo "--- Build image"
 $(dirname $0)/../build.sh $GRAFANA_VERSION $DOCKER_IMAGE $VERSION
+
 if [ "$PUSH_IMAGE" == "true" ]; then
   $(dirname $0)/push.sh
 fi;
--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=${2:-local/grafana}
 VERSION=${3:-local}
 
 cat Dockerfile | sed "s/<VERSION>/$GRAFANA_VERSION/" > Dockerfile.tmp
-docker build -f Dockerfile.tmp -t $DOCKER_IMAGE:$VERSION .
+docker build -f Dockerfile.tmp -t $DOCKER_IMAGE .
