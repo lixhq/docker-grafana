039a16588679c5f0d181f595a0788c507f65ad55
Peter Majchrak <pm@lix.com>
2018-05-14 14:46:09 +0200

    Fix copy paste

--- .buildkite/push.sh
+++ .buildkite/push.sh
@@ -3,6 +3,6 @@ set -e
 
 eval "$($(dirname $0)/env.sh)"
 
-echo "--- Push elasticsearch image"
+echo "--- Push image"
 docker tag "$DOCKER_IMAGE" "quay.io/lixtech/$REPO:$FULL_VERSION"
 docker push "quay.io/lixtech/$REPO:$FULL_VERSION"
